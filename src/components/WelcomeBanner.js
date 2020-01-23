1202a4312a8f438d149267b70cb04ef4a40e0493
Ezra Moore <Ezra.Moore@guard.com>
2020-01-23 13:30:56 -0500

    added carousel fixed queries

--- src/components/WelcomeBanner.js
+++ src/components/WelcomeBanner.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import { graphql, StaticQuery } from 'graphql';
+import { graphql, StaticQuery } from 'gatsby';
+// Requires react-responsive-carousel from npm
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 import styled from 'styled-components';
 
-const WelcomeBanner = ({pageContext}) => (
-    <div>
-        asdfasdf
-    </div>
+const MainImage = styled.img`
+    width: 100%;
+    height: 500px;
+    object-fit: contain;
+`;
+
+const WelcomeBanner = () => (
+    /* Requires media library categories plugin */
+    <StaticQuery query={graphql`
+        {
+            allWordpressWpMedia(filter: {categories: {elemMatch: {name: {eq: "Carousel"}}}}) {
+                edges {
+                  node {
+                    media_type
+                    path
+                    source_url
+                    caption
+                    categories {
+                      name
+                    }
+                  }
+                }
+              }
+        }
+    `} render={props => (
+            <Carousel autoPlay={true} showThumbs={false} showIndicators={false} showStatus={false}>
+                {props.allWordpressWpMedia.edges.map(elem => (
+                    <div>
+                        <MainImage src={elem.node.source_url} alt={elem.node.media_type} />
+                    </div>
+                ))}
+            </Carousel>
+    )} />
 );
 
 export default WelcomeBanner;
